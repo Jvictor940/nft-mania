29cffded7fbaee9d4ba8ec87d57eefc81ac3124c
Jacky Liang <asheriffy@gmail.com>
2022-04-18 20:58:34 -0400

    initial branch commit. not finished

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 function About() {
     return( 
         <div>
-
+            <h1>About Our Project</h1>
+            <p>Nft Mania is about minting quality quality pictures rather than 16-bit monkeys</p>
         </div>
     )
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Form() {
-    return( 
-        <div>
+function Form({ setPictures }) {
 
-        </div>
-    )
+const [form, setForm] = useState({imgURL: '', tags: '', views: 0, likes: 0, user:'', amount: 0})
+
+const updateForm = (e) => {
+    setForm({...form, [e.target.getAttribute('name')]: e.target.value})
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    let req = await fetch('http://localhost:8000/pictures', {
+        method: 'POST',
+        headers: {'CONTENT-TYPE':'application/json'},
+        body: JSON.stringify(form)
+      })
+    let res = await req.json()
+    console.log("res is", res, typeof res)
+     
+    setPictures((prevState) => { return [...prevState, res]})
+}
+
+    return (
+        <form onSubmit={handleSubmit} className="new-picture-form">
+          <input type="text" onChange={(e) => updateForm(e)} name="webformatURL" placeholder="Image URL" value={imgURL} />
+          <input type="text" onChange={(e) => updateForm(e)} name="tags" placeholder="Tags" value={tags}/>
+          <input type="number" onChange={(e) => updateForm(e)} name="amount" placeholder="Price" value={price}/>
+          <input type="text" onChange={(e) => updateForm(e)} name="user" placeholder="Created By" value={user}/> 
+          <input type="submit" value="Create your own NFT" />
+        </form>
+      );
 }
 export default Form
