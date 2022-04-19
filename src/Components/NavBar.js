68555f754a6cbfceb16100611d5d8d2d59b39d10
Ruslan Khait <rusty6916@gmail.com>
2022-04-19 16:19:41 -0400

    styling the navbar component

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,13 @@
+import './NavBar.css'
 import React from 'react'
 import { NavLink } from "react-router-dom"
 
 function NavBar() {
     return( 
-        <div>
-            <NavLink to="/">NFT Mania</NavLink>
-            <NavLink to="/create">Create an NFT</NavLink>
-            <NavLink to="/about">About</NavLink>
+        <div className="nav-menu">
+            <NavLink to="/" className="nav-home">NFT Mania</NavLink>
+            <NavLink to="/create" className="nav-form">Create an NFT</NavLink>
+            <NavLink to="/about" className="nav-about">About</NavLink>
         </div>
     )
 }
