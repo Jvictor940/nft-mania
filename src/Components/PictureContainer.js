b876e5e7b06248d3c48eaa95d3c15f2dd9261540
Jerry <Jvictor940@gmail.com>
2022-04-19 14:45:17 -0400

    search feature

--- src/Components/PictureContainer.js
+++ src/Components/PictureContainer.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Picture from './Picture'
 import Search from './Search'
 
 function PictureContainer({pictures}) {
+    const [search, setSearch] = useState([])
+    const displayedPictures = pictures.filter(picture => picture.tags.includes(search))
+    
+    function handleSearchChange(newSearch) {
+        setSearch(newSearch)
+        console.log(newSearch)
+    }
+    
+
     return( 
         <div> 
-            <Search />
+            <Search search={search} onSearchChange={handleSearchChange} />
             {
-               pictures.map((element) => {
+               displayedPictures.map((element) => {
                    return(
                        <Picture key={element.id} picture={element} />
                    )
