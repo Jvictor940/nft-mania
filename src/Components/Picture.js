79fb066fab0f27fa7a80b99a8b8c6191addd3987
Ruslan Khait <rusty6916@gmail.com>
2022-04-20 15:02:33 -0400

    Change DOGE style

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -5,13 +5,11 @@ function Picture({picture}) {
         <div className="picture-item">
             <img src={picture.webformatURL} />
             <div className="picture-tag">
-                <p><strong><span className="span-tags">Tags:</span></strong> <i>{picture.tags}</i></p>
-            </div>
-            <div className="picture-desc">
-                <p>Seller: {picture.user}</p>
-                <p>Views: {picture.views}</p>
-                <p>Likes: {picture.likes} <button>♡</button></p>
-                <p>Price: {picture.id} DOGE</p>
+                <p><strong><span className="span-tags">Tags:</span></strong> <i>{picture.tags}</i></p> 
+                <p><strong><span className="span-tags">Seller:</span></strong> <i>{picture.user}</i></p>
+                <p><strong><span className="span-tags">Views:</span></strong> <i>{picture.views}</i></p>
+                <p><strong><span className="span-tags">Likes:</span></strong><i>{picture.likes}</i> <button>♡</button></p>
+                <p><strong><span className="span-tags">Price:</span></strong><i>{picture.id} <span className='doge-span'>DOGE</span></i></p>
                 <button className="buy-btn">Buy</button>
             </div>
         </div>    
