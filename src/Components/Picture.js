ac5e64b6089c6915abdc39a0f77f8502740c04c7
Ruslan Khait <rusty6916@gmail.com>
2022-04-20 14:43:33 -0400

    filled in picture infor and added rows

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -4,6 +4,16 @@ function Picture({picture}) {
     return( 
         <div className="picture-item">
             <img src={picture.webformatURL} />
+            <div className="picture-tag">
+                <p><strong><span className="span-tags">Tags:</span></strong> <i>{picture.tags}</i></p>
+            </div>
+            <div className="picture-desc">
+                <p>Seller: {picture.user}</p>
+                <p>Views: {picture.views}</p>
+                <p>Likes: {picture.likes} <button>♡</button></p>
+                <p>Price: {picture.id} DOGE</p>
+                <button className="buy-btn">Buy</button>
+            </div>
         </div>    
     )
 }
