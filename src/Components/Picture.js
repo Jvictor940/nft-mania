3f37415f506b02900568a7c66ef1cbe8ac2fcbe4
Jerry <Jvictor940@gmail.com>
2022-04-20 16:11:32 -0400

    added heart functionality

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Picture({picture}) {
+    const [likes, setLikes] = useState(picture.likes)
+    const [isLike, setIsLike] = useState(false)
+
+    function handleClick() {
+        setIsLike(!isLike)
+        isLike ? setLikes(likes - 1) : setLikes(likes + 1)
+    }
+
     return( 
         <div className="picture-item">
             <img src={picture.webformatURL} />
@@ -8,7 +16,7 @@ function Picture({picture}) {
                 <p><strong><span className="span-tags">Tags:</span></strong> <i>{picture.tags}</i></p> 
                 <p><strong><span className="span-tags">Seller:</span></strong> <i>{picture.user}</i></p>
                 <p><strong><span className="span-tags">Views:</span></strong> <i>{picture.views}</i></p>
-                <p><strong><span className="span-tags">Likes:</span></strong><i>{picture.likes}</i> <button>♡</button></p>
+                <p><strong><span className="span-tags">Likes:</span></strong><i> {likes}</i> <button className='like-btn' onClick={handleClick}>{isLike ? '❤️' : '♡'}</button></p>
                 <p><strong><span className="span-tags">Price:</span></strong><i>{picture.id} <span className='doge-span'>DOGE</span></i></p>
                 <button className="buy-btn">Buy</button>
             </div>
@@ -16,3 +24,5 @@ function Picture({picture}) {
     )
 }
 export default Picture
+
+// ❤️
