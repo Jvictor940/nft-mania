ff4fff23a86098b815a7e2ba83c3d10f21fc516c
Jerry <Jvictor940@gmail.com>
2022-04-21 19:45:28 -0400

    modified about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,10 +12,10 @@ function About() {
             </p>
             </div>
             <div className='about-images-container'>
-            <img className='about-images' src='https://earthsky.org/upl/2021/10/Full-Hunters-Moon_Gizaw-Legesse_Addis-Ababa-Ethiopia_Oct-20-2021-e1634780880574.jpg'
+            {/* <img className='about-images' src='https://earthsky.org/upl/2021/10/Full-Hunters-Moon_Gizaw-Legesse_Addis-Ababa-Ethiopia_Oct-20-2021-e1634780880574.jpg'
             alt='full-moon' />
             <img className='about-images' src='https://media.istockphoto.com/photos/space-shuttle-rocket-launch-in-the-sky-and-clouds-to-outer-space-sky-picture-id1344443930?b=1&k=20&m=1344443930&s=170667a&w=0&h=OFxY7InQfBGsBLkPuUBZECWkS3H9kc3rY1O2FaahXvo='
-            alt ='rocket-ship' />
+            alt ='rocket-ship' /> */}
             </div>
         </div>
     )
