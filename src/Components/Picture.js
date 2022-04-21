e6e61af3ca50cf5eeadfcc1867f76a759c2b4af4
Ruslan Khait <rusty6916@gmail.com>
2022-04-21 10:10:38 -0400

    fixed position of the price text and the DOGECOIN icon

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -22,7 +22,7 @@ function Picture({picture}) {
                 <p><strong><span className="span-tags">Seller:</span></strong> <i>{picture.user}</i></p>
                 <p><strong><span className="span-tags">Views:</span></strong> <i>{picture.views}</i></p>
                 <p><strong><span className="span-tags">Likes:</span></strong><i> {likes}</i> <button className='like-btn' onClick={handleClick}>{isLike ? '❤️' : '♡'}</button></p>
-                <p><strong><span className="span-tags">Price:</span></strong><i> {picture.id} <span className='doge-span'> <img src="https://www.shareicon.net/data/256x256/2015/09/14/101012_doge_512x512.png" className='doge-img'/> </span></i></p>
+                <p className="picture-price"><strong><span className="span-tags">Price:</span></strong><i> {picture.id} <span className='doge-span'> <img src="https://www.shareicon.net/data/256x256/2015/09/14/101012_doge_512x512.png" className='doge-img'/> </span></i></p>
                 <button className={isSold ? "sold-btn" : "buy-btn"} onClick={handleSold}>{isSold ? 'Sold' : 'Buy' }</button>
             </div>
         </div>    
