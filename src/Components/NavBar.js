5eff4df79e18b7d2f4dcca3f60bedbc43cb8478e
Ruslan Khait <rusty6916@gmail.com>
2022-04-20 20:41:10 -0400

    add nft punk as an icon on navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom"
 function NavBar() {
     return( 
         <div className="nav-menu">
+            <img src="https://f8n-production.s3.amazonaws.com/creators/profile/ooryfc0yg-4389-256x256-png-anc8fa.png" className="nft-icon"/>
             <NavLink to="/" className="nav-home">NFT Mania</NavLink>
             <NavLink to="/create" className="nav-form">Create an NFT</NavLink>
             <NavLink to="/about" className="nav-about">About</NavLink>
