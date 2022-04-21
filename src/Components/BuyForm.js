19496cf120825c08a2e7668a71a6359ece3bb3d9
Jacky Liang <asheriffy@gmail.com>
2022-04-21 10:52:06 -0400

    added buyform

---
+++ src/Components/BuyForm.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default BuyForm () => {
+    return (
+        <div className="buy-form">
+            <h1>Buy Form text</h1>
+        </div>
+    )
+}
